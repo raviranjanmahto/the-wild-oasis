f85c58f41a4480e3847f907d193b64c5bd1b9458
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-03-06 02:40:32 +0530

    CabinTable row apiCall and Spinner for Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Bookings from './pages/Bookings';
-import Cabins from './pages/Cabins';
-import Users from './pages/Users';
-import Settings from './pages/Settings';
-import Account from './pages/Account';
-import Login from './pages/Login';
-import PageNotFound from './pages/PageNotFound';
-import GlobalStyles from './styles/GlobalStyles';
-import AppLayout from './ui/AppLayout';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTimeLimit: 60 * 1000,
+      staleTimeLimit: 60,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +42,8 @@ const App = () => {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   );
 };
 
